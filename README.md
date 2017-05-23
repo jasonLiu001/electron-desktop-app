7e5167b51560f2cfe493ef1f36c129d8dbb162ec
wangliu <wangliu@isoftstone.com>
2017-05-23 20:52:02 +0800

    project init

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
 # 项目说明
 
 ## 模块依赖
-`Package.json`需要添加以下模块
+`Package.json` 除了添加`angular`的相关依赖项外，还需要添加以下依赖模块，`angular`的模块依赖如下：
+```shell
+npm install @angular/common @angular/compiler @angular/core @angular/forms @angular/http @angular/platform-browser @angular/platform-browser-dynamic @angular/router angular-in-memory-web-api rxjs zone.js --save
+```
+
+ 额外的依赖模块如下：
 
 ```shell
 npm install typescript --save-dev
@@ -9,4 +14,8 @@ npm install electron --save-dev
 npm install @types/electron --save-dev
 npm install @types/node --save-dev
 ```
+
+## 开发遇到问题
+
+`angular`模板中不能直接写`script`脚本，`electron`开发时，有在`view`中直接写`script`的需要
 
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
-import {app, BrowserWindow} from "electron";
+const {app, BrowserWindow} = require("electron");
 
-let path = require('path'),
+const path = require('path'),
     url = require('url');
 
 // Keep a global reference of the window object, if you don't, the window will
@@ -26,7 +26,7 @@ function createWindow(): void {
         // Dereference the window object, usually you would store windows
         // in an array if your app supports multi windows, this is the time
         // when you should delete the corresponding element.
-        win = null
+        win = null;
     });
 }
 
@@ -41,7 +41,7 @@ app.on('window-all-closed', () => {
     // On macOS it is common for applications and their menu bar
     // to stay active until the user quits explicitly with Cmd + Q
     if (process.platform !== 'darwin') {
-        app.quit()
+        app.quit();
     }
 });
 
@@ -49,11 +49,9 @@ app.on('activate', () => {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
     if (win === null) {
-        createWindow()
+        createWindow();
     }
 });
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
-
-
